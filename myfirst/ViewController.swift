c19910599e663f5dbd31ac46da0f0c31f9452dbc
sourceinn <sourceinn@huangxiangguodeMac-mini.local>
2017-11-05 09:03:47 +0800

    view練習

--- myfirst/ViewController.swift
+++ myfirst/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
+
 
